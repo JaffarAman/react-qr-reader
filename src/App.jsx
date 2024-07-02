165a14636060dbe6bd690d6d0f5dd690abacf807
Jaffar Aman <amanjaffar50@gmail.com>
2024-07-03 00:29:56 +0500

    qr code app

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+// class Test extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       delay: 300,
+//       result: "No result",
+//     };
+//     this.handleScan = this.handleScan.bind(this);
+//   }
+// }
+
+import React from "react";
+import { useState } from "react";
+import { QrReader } from "react-qr-reader";
+
+const App = () => {
+  const [delay, setDelay] = useState(100);
+  const [result, setresult] = useState("No result");
+  const [data, setData] = useState("No result");
+  const handleScan = (data) => {
+    console.log("hahahahah", data);
+    if (data) {
+      console.log(data, "data");
+      setresult(data);
+    }
+  };
+  function handleError(err) {
+    console.error(err);
+  }
+
+  return (
+    <div style={{ width: "300px" }}>
+      <QrReader
+        ViewFinder={Finder}
+        scanDelay={600}
+        constraints={{
+          ConstrainDouble: {
+            max: "20px",
+          },
+        }}
+        onResult={(result, error) => {
+          if (!!result) {
+            setData(result?.text);
+          }
+
+          if (!!error) {
+            console.info("error", error);
+          }
+        }}
+        style={{ width: "50%" }}
+      />
+      <p>{data}</p>
+    </div>
+  );
+};
+
+const Finder = () => <h1>hELLO WOLRRR</h1>;
+export default App;
